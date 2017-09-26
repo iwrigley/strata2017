92b5fbae50fa518e2fce5ba1b9968ca78315f875
Ian Wrigley <ian@StreamBook.local>
2017-09-25 23:16:51 -0400

    first commit

---
+++ streams/src/main/java/streamdemo/StreamsExercise.java
@@ -0,0 +1,44 @@
+package solution;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+
+public class StreamDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        Properties streamsConfiguration = new Properties();
+
+        // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-exercise1");
+
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "broker101:9092");
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Specify default (de)serializers for record keys and for record values.
+        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        KStreamBuilder builder = new KStreamBuilder();
+
+        // Read the input Kafka Topic into a KStream instance.
+        KStream<String, String> dm = builder.stream("dmesg");
+
+        // Convert the values to upper case
+        KStream<String, String> uppercased = dm.mapValues(String::toUpperCase);
+
+        // Write the uppercased results to a new Kafka Topic called "UppercasedDmsg".
+        uppercased.to("UppercasedDmsg");
+
+        KafkaStreams streams = new KafkaStreams(builder, streamsConfiguration);
+        streams.start();
+    }
+
+}
+
